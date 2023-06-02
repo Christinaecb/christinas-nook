4e905ef11b1e2b85cfb3ee2f9ca8329d4d4b9600
Christina <christina.e.c.barberi@gmail.com>
2023-06-02 16:30:12 -0400

    imported modules

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,8 @@
+// Import the 'typeDefs' module from the './typeDefs' file
 const typeDefs = require('./typeDefs');
+
+// Import the 'resolvers' module from the './resolvers' file
 const resolvers = require('./resolvers');
 
+// Export the 'typeDefs' and 'resolvers' objects
 module.exports = { typeDefs, resolvers };
