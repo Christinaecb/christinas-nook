84ed25eb61efb4aea44e51287a4c3b815b0713c7
Christina <christina.e.c.barberi@gmail.com>
2023-06-02 16:02:33 -0400

    added saveBookcapture and utilize response from saveBook function

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -50,12 +50,9 @@ const SearchBooks = () => {
     }
   };
   
-    // create function to handle saving a book to our database
     const handleSaveBook = async (bookId) => {
       // find the book in `searchedBooks` state by the matching id
       const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-  
-      // get token
       const token = Auth.loggedIn() ? Auth.getToken() : null;
   
       if (!token) {
@@ -63,12 +60,11 @@ const SearchBooks = () => {
       }
   
       try {
-        await saveBook({
+        const { data } = await saveBook({
           variables: { newBook: { ...bookToSave } },
         });
   
-        // if book successfully saves to user's account, save book id to state
-        setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+        setSavedBookIds([...savedBookIds, data.saveBook.bookId]);
       } catch (err) {
         console.error(err);
       }
